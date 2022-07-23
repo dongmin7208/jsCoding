efb78fcf67c44b129b976948956e6e6fc8be578c
dongmin7208 <sayg3o3d@gmail.com>
2022-07-23 13:47:58 +0900

    격자판

--- section2/6.js
+++ section2/6.js
@@ -7,8 +7,14 @@ function solution(arr) {
             sum1 += arr[i][j];
             sum2 += arr[j][i];
         }
+        answer = Math.max(answer, sum1, sum2);
     }
-
+    sum1 = sum2 = 0;
+    for (let i = 0; i < n; i++) {
+        sum1 += arr[i][i];
+        sum2 += arr[i][n - i - 1];
+    }
+    answer = Math.max(answer, sum1, sum2);
     return answer;
 }
 
